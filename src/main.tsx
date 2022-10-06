6aeaad2bcdba705efbdff97aed927eb3d054d44d
ghoon99 <ghoon99@kakao.com>
2022-10-06 18:40:29 +0900

    fix: index.ts import 경로 오류 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { Route, Router } from "@components";
-import { Root, About } from "@pages";
+import { Route } from "@components/Route";
+import { Router } from "@components/Router";
+import { Root } from "@pages/Root";
+import { About } from "@pages/About";
+
 import "./index.css";
 
 const container = document.getElementById("root") as HTMLElement;
