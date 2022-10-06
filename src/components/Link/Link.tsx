869345da77288d9952303f738ddcb103ddfcd4e1
ghoon99 <ghoon99@kakao.com>
2022-10-06 22:00:46 +0900

    refactor: 중복되는 useContext useRouter 훅으로 변경

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,5 +1,5 @@
-import { useContext, type ReactNode, type MouseEvent } from "react";
-import { RouterContext } from "@components/Router";
+import { type ReactNode, type MouseEvent } from "react";
+import { useRouter } from "@hooks/useRouter";
 
 interface LinkProps {
   children: ReactNode;
@@ -8,14 +8,12 @@ interface LinkProps {
 
 /* a 태그의 기본동작을 막고 path 상태를 업데이트 시켜야함 */
 const Link = ({ to, children }: LinkProps) => {
-  const value = useContext(RouterContext);
-  if (value === null) throw new Error("Router컴포넌트 내부에서 사용해주세요!");
+  const router = useRouter();
 
-  const { changePath } = value;
   const handleLinkClick = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     // currentPath를 to 로 변경시겨야 함
-    changePath(to);
+    router.push(to);
   };
 
   return (
--- src/components/Route/Route.tsx
+++ src/components/Route/Route.tsx
@@ -1,5 +1,5 @@
-import { type ReactNode, useContext } from "react";
-import { RouterContext } from "@components/Router";
+import { type ReactNode } from "react";
+import { useRouter } from "@hooks/useRouter";
 
 interface RouterProps {
   path: string;
@@ -8,11 +8,8 @@ interface RouterProps {
 
 /* 지정한 path와 현재 경로가 같을 때 컴포넌트를 렌더링한다. */
 const Route = ({ path, component }: RouterProps) => {
-  const value = useContext(RouterContext);
-  if (value === null) throw new Error("Router컴포넌트 내부에서 사용해주세요!"); // 어? 공통?
-
-  const { currentPath } = value;
-  if (currentPath !== path) return null;
+  const router = useRouter();
+  if (router.pathname !== path) return null;
 
   return <>{component}</>;
 };
