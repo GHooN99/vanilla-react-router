c531b00283087f745dab3df45e0c900adbb4570a
ghoon99 <ghoon99@kakao.com>
2022-10-06 18:31:03 +0900

    config: 프로젝트 초기세팅(eslint,prettier,tsconfig, 폴더 구조) 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Route, Router } from "@components";
+import { Root, About } from "@pages";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const container = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(container).render(
+  <Router>
+    <Route path="/" component={<Root />} />
+    <Route path="/about" component={<About />} />
+  </Router>
+);
