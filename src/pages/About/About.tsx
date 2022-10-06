b7d34b321e4dcd796b5b38c8c9ab798385416cb3
ghoon99 <ghoon99@kakao.com>
2022-10-06 23:08:22 +0900

    feat: useRouter 를 이용한 페이지 이동 버튼 추가

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,14 @@
 import { Link } from "@components/Link";
+import { useRouter } from "@hooks/useRouter";
 
 const About = () => {
+  const router = useRouter();
+
   return (
     <div>
       <h1>About</h1>
-      <Link to="/">go main</Link>
+      <Link to="/">go main (Link)</Link>
+      <button onClick={() => router.push("/")}>go main (router.push)</button>
     </div>
   );
 };
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,10 +1,14 @@
 import { Link } from "@components/Link";
+import { useRouter } from "@hooks/useRouter";
 
 const Root = () => {
+  const router = useRouter();
+
   return (
     <div>
       <h1>Root</h1>
-      <Link to="/about">about</Link>
+      <Link to="/about">about (Link)</Link>
+      <button onClick={() => router.push("/about")}>about (router.push)</button>
     </div>
   );
 };
