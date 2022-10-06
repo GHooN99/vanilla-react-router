eabd9d63c485fa905ed18f9cc0205bd95ff8e4ab
ghoon99 <ghoon99@kakao.com>
2022-10-06 18:56:16 +0900

    feat: Route 컴포넌트 초기 구현

--- src/components/Route/Route.tsx
+++ src/components/Route/Route.tsx
@@ -1,5 +1,15 @@
-const Route = () => {
-  return <div></div>;
+import type { ReactNode } from "react";
+
+interface RouterProps {
+  path: string;
+  component: ReactNode;
+}
+
+/* 지정한 path와 현재 경로가 같을 때 지정한 컴포넌트를 렌더링한다. */
+const Route = ({ path, component }: RouterProps) => {
+  if (window.location.pathname !== path) return null;
+
+  return <>{component}</>;
 };
 
 export default Route;
